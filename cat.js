b43fe84056b7e1368d2641ccf34a7760ac3180ac
MagdK <kovacsmagda@gmail.com>
2022-02-07 19:19:38 +0100

    Change name of JS file about cat, remove body shaming question about my cat

---
+++ cat.js
@@ -0,0 +1,30 @@
+// Data about Effi
+const nameOfCatNumberOne = "Effi";
+const countryOfOrigin = "Hungary";
+const bornInYear = 2014;
+const color = "Ginger";
+const isKidFriendly = true;
+const isVaccinated = true;
+const fromAShelter = true;
+const likesCatNumberTwo = false;
+
+const preferences = {
+    "Favorite food": "Ruccola",
+    "Favorite game": "Fishing rod",
+    "Favorite place to sleep": "Home office shelf on heating"
+}
+ 
+const infoAboutCatNumberOne = { 
+    "Cat's name": nameOfCatNumberOne, 
+    "Country of origin": countryOfOrigin, 
+    "Age": 2022 - bornInYear,
+    "Color": color,
+    "Is she kid friendly?": isKidFriendly, 
+    "Is she vaccinated?": isVaccinated,
+    "Was she adopted from a shelter?": fromAShelter,
+    "Likes Dena (cat number two)?": likesCatNumberTwo,
+    "What does she like?": preferences
+}
+
+console.log(infoAboutCatNumberOne);
+
