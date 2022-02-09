9f43f5ac1523206bb6552f33911aff6eaaf5a1ba
Magda Kovács <kovacsmagda@gmail.com>
2022-02-09 08:50:24 +0100

    Syntax change in facebook.js

--- facebook.js
+++ facebook.js
@@ -12,10 +12,28 @@ const myProfile = {
         {"name": "Jane Doe", "age": 20},
         {"name": "Janet Doe", "age": 45}
     ],
+
     "likes": [
         {"url": "https://kittens.com", "date": new Date(2015, 08, 15)},
         {"url": "https://puppies.com", "date": new Date(2016, 04, 15)},
-        {"url": "https://horses.com", "date": new Date(2017, 07, 19)},
+        {"url": "https://horses.com", "date": new Date(2017, 07, 19)}
+    ],
+
+    "family":[ 
+        {"mother": "Jenny Doe", "born": 1950},
+        {"father": "Johathan Doe", "born": 1940},
+        {"sister": "Jolien Doe", "born": 1980},
+        {"brother": "James Doe", "born": 1990}
+    ],
+
+    "life events": [
+        {"born": 1985},
+        {"graduated from elementary school": 2001},
+        {"graduated from highschool": 2005},
+        {"graduated from college": 2011},
+        {"job 1": 2011, "company name": "Tupturup"},
+        {"job 2": 2013, "company name": "Csiripcsirip Kft."},
+        {"job 3": 2013, "company name": "Pamparam Bt."}
     ]
 }
 
